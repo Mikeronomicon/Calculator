a84e3300e5c9423756a879f4870d4db2881e20df
Curt Poff <curt.poff@gmail.com>
2015-05-07 16:10:10 -0700

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,31 +1,32 @@
 var a, b, result;
-     function setValues()
-     {
-          a = Number(document.getElementById(“a”).value);
-          b = Number(document.getElementById(“b”).value);
-     }
+                              function setValues()
+                              {
+                                             a = Number(document.getElementById("a").value);
+                                             b = Number(document.getElementById("b").value);
+                              }
+ 
+                              function addition()
+                              {
+                                             setValues();
+                                             result = a + b;
+                                             alert("The result is = " + result);
+                              }
+                              function subtraction()
+                              {
+                                             setValues();
+                                             result = a - b;
+                                             alert("The result is = " + result);
+                              }
+                              function multiplication()
+                              {
+                                             setValues();
+                                             result = a * b;
+                                             alert("The result is = " + result);
+                              }
+                              function division()
+                              {
+                                             setValues();
+                                             result = a / b;
+                                             alert("The result is = " + result);
+                              }
 
-     function addition()
-     {
-          setValues();
-          result = a + b;
-          alert(“The result is = ” + result);
-     }
-     function subtraction()
-     {
-          setValues();
-          result = a - b;
-          alert(“The result is = ” + result);
-     }
-     function multiplication()
-     {
-          setValues();
-          result = a * b;
-          alert(“The result is = ” + result);
-     }
-     function division()
-     {
-          setValues();
-          result = a / b;
-          alert(“The result is = ” + result);
-     }
