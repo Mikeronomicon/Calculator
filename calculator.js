92a960dcce8fa9e51d9a86337b6492326f0c76bf
Michael <[mlorenzo@unrulyvegetables.com]>
2015-05-07 16:49:30 -0700

    still trying to get the clear button to work, all other buttons and math are working.

--- calculator.js
+++ calculator.js
@@ -1,31 +1,37 @@
 var a, b, result;
      function setValues()
      {
-          a = Number(document.getElementById(“a”).value);
-          b = Number(document.getElementById(“b”).value);
+          a = Number(document.getElementById("a").value);
+          b = Number(document.getElementById("b").value);
      }
 
      function addition()
      {
           setValues();
           result = a + b;
-          alert(“The result is = ” + result);
+          alert(result);
      }
      function subtraction()
      {
           setValues();
           result = a - b;
-          alert(“The result is = ” + result);
+          alert(result);
      }
      function multiplication()
      {
           setValues();
           result = a * b;
-          alert(“The result is = ” + result);
+          alert(result);
      }
      function division()
      {
           setValues();
           result = a / b;
-          alert(“The result is = ” + result);
+          alert(result);
      }
+     function clear()
+     {
+          document.getElementById("a").value = "";
+          document.getElementById("a").value = "";
+     }
+     
