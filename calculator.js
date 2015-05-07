0ef3f603e74f8f15c6570226a78b41a1572792ef
Michael <[mlorenzo@unrulyvegetables.com]>
2015-05-07 16:06:10 -0700

    cleaned up code and formatted better

--- calculator.js
+++ calculator.js
@@ -1,31 +1,31 @@
 var a, b, result;
-                              function setValues()
-                              {
-                                             a = Number(document.getElementById(“a”).value);
-                                             b = Number(document.getElementById(“b”).value);
-                              }
- 
-                              function addition()
-                              {
-                                             setValues();
-                                             result = a+b;
-                                             alert(“The result is = ” + result);
-                              }
-                              function subtraction()
-                              {
-                                             setValues();
-                                             result =a-b;
-                                             alert(“The result is =” + result);
-                              }
-                              function multiplication()
-                              {
-                                             setValues();
-                                             result =a*b;
-                                             alert(“The result is =” + result);
-                              }
-                              function division()
-                              {
-                                             setValues();
-                                             result =a/b;
-                                             alert(“The result is =” + result);
-                              }
+     function setValues()
+     {
+          a = Number(document.getElementById(“a”).value);
+          b = Number(document.getElementById(“b”).value);
+     }
+
+     function addition()
+     {
+          setValues();
+          result = a + b;
+          alert(“The result is = ” + result);
+     }
+     function subtraction()
+     {
+          setValues();
+          result = a - b;
+          alert(“The result is = ” + result);
+     }
+     function multiplication()
+     {
+          setValues();
+          result = a * b;
+          alert(“The result is = ” + result);
+     }
+     function division()
+     {
+          setValues();
+          result = a / b;
+          alert(“The result is = ” + result);
+     }
